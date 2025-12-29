e772b8d4527994971861e5acbe062e9105440c21
Oleksii Zaivenko <alexey.zaivenko@gmail.com>
2025-12-29 17:34:38 +0100

    parsing response data into vars

--- url-fetch/index.ts
+++ url-fetch/index.ts
@@ -1,7 +1,17 @@
 import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
-
+// response data has properties: userId, id, title, completed
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data;
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+// easy to make a mistake with response properties
+  console.log(`
+    The Todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}
+  `);
 });
